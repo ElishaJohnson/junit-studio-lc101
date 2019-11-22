aa1faf3a3cf15f932e6fc5e3a2d89dffb156e066
Elisha Johnson <elishamagicjohnson@gmail.com>
2019-11-21 18:03:28 -0600

    chapter 6 studio

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,10 +1,13 @@
 package test;
 
+import main.BalancedBrackets;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class BalancedBracketsTest {
+    BalancedBrackets test_Class = new BalancedBrackets();
 
     //TODO: add tests here
     @Test
@@ -12,5 +15,28 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(test_Class.hasBalancedBrackets("[]"));
+    }
 
+    @Test
+    public void onlyLeftBracketReturnsFalse() {
+        assertFalse(test_Class.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void wrongOrderedBracketsReturnsFalse() {
+        assertFalse(test_Class.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void longImbalancedStringReturnsFalse() {
+        assertFalse(test_Class.hasBalancedBrackets("LaunchCode[0][[7]"));
+    }
+
+    @Test
+    public void longBalancedStringReturnsTrue() {
+        assertTrue(test_Class.hasBalancedBrackets("[LaunchCode[[0, 5], [3, 4]]]"));
+    }
 }
